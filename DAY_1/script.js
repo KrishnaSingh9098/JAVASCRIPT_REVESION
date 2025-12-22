6fd54b852a2a3a70c2a8a295eb26adc7820c134f
KrishnaSingh9098 <arushsingh5103@gmail.com>
2025-12-22 20:52:55 +0530

    Revesion

---
+++ DAY_1/script.js
@@ -0,0 +1,13 @@
+let userName = 'Krishna';
+let lastName = 'Singh';
+let age = 25;
+
+let isHappy = true;
+let a; // undefined
+
+let userIntro = "Hello, my name is " + userName + " " + lastName + ". My age is " + age;
+
+alert(userIntro);
+
+const b = prompt("Enter your name:");
+console.log("User entered:", b);
