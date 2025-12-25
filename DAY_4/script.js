0ea782e2ecd27e43c054ff7ba84a133ef73284bc
KrishnaSingh9098 <arushsingh5103@gmail.com>
2025-12-25 08:45:43 +0530

    MATHS AND THIER PROPERTIES

---
+++ DAY_4/script.js
@@ -0,0 +1,14 @@
+console.log("Hello World")
+
+
+// # ARea Of Ractangle : 
+
+let length = +prompt("Enter the length")
+let bidth =  +prompt("Enter the bidth")
+
+let areaOfRectangle= length*bidth
+console.log(areaOfRectangle)
+
+
+
+document.write()
