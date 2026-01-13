d7686df5cd7df1bd02c029bfac429256cce47173
KrishnaSingh9098 <arushsingh5103@gmail.com>
2026-01-13 20:52:05 +0530

    HELLO

---
+++ DAY_9/script.js
@@ -0,0 +1,18 @@
+
+const user = {
+    firstName : 'Krishna',
+    lastname: 'Singh',
+    pata:{
+        city:'Banglore',
+        pinCode : 895476,
+        state : 'Karnataka',
+        moreDetails : {
+            population : 59859534032132,
+            area: '4785 sq km'
+        }
+    },
+    age : 29,
+     isGraduate : false
+}
+
+let userName =' Anuragh'
